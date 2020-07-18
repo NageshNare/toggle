49ef7fe89976b088e5a0dc015e573f104c816706
Nagorao Nare <nagorao.nare@isobar.com>
2020-07-18 20:34:01 +0530

    new files added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Table from './Table';
+import Chart from './Chart';
+
+class App extends Component {
+
+  state = {
+    toggle: true
+  }
+  toggleHandler = () =>{
+
+    let t = this.state.toggle ? false : true;
+
+    this.setState({toggle: t})
+  }
+  render(){
+
+    let post = this.state.toggle ? <Table /> : <Chart />;
+    return (
+      <div className="App">
+        <button onClick={this.toggleHandler}>Click</button>
+        {post}
+      </div>
+    );
+  } 
 }
 
 export default App;
---
+++ src/Chart.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class Chart extends Component{
+
+    render(){
+        return(
+            <div><h1>Chart Component</h1></div>
+        );
+    }
+}
+
+export default Chart;
